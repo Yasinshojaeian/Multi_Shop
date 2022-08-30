b4f5890cf8b65df31fd54ffc8373bd73dc4f93a6
yasin <yasinmohamad4831@gmail.com>
2022-08-30 07:50:14 +0430

    Start Sms in Project

--- account/forms.py
+++ account/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import User
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-
+from django.core import validators
 
 class UserCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
@@ -42,9 +42,52 @@ class UserChangeForm(forms.ModelForm):
         model = User
         fields = ('phone', 'password', 'is_active', 'is_admin')
         
-        
-        
-        
+
+def validations_phone(value):
+    if len(value) > 12:
+        raise ValidationError('تلفن شما معتبر نمی باشد')
+    if value[0] != '0':
+        raise ValidationError('تلفن شما باید با 0 شروع بشود')
+
+
+
 class LoginForm(forms.Form):
     phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'} ),label='شماره تلفن')
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),label='رمز عبور')
+
+    # def clean_phone(self):
+    #     phone = self.cleaned_data.get('phone')
+    #     if len(phone) > 11 :
+    #         raise ValidationError(
+    #             'Invalid value: %(value)s is invaliad',
+    #             code='invalid',
+    #             params={'value': f'{phone}'},
+    #         )
+    #     return phone
+    def clean_phone(self):
+
+        cd = super().clean()
+        phone = cd['phone']
+        if len(phone) > 12:
+            self.add_error('phone','Phone Is Not INvalid')
+        if phone[0] != '0':
+            self.add_error('phone','شماره شما باید با 09 شروع شود ')
+        if phone[1] != '9':
+            p = phone[1]
+            raise ValidationError(
+                        'Invalid value: %(value)s',
+                        code='invalid',
+                        params={'value': f'رقم دوم شماره شما باید به جای{p} عدد 9 باشد'},
+                    )
+        return phone
+    # def clean(self):
+    #     breakpoint()
+    #     y = super(LoginForm, self).clean()
+    #     cd = super().clean()
+    #     phone = cd['phone']
+    #     if len(phone) > 11 :
+    #         raise ValidationError(
+    #             'Invalid value: %(value)s is invaliad',
+    #             code='invalid',
+    #             params={'value': f'{phone}'},
+    #         )
---
+++ account/sms.py
@@ -0,0 +1,5 @@
+import ghasedakpack
+
+
+sms=ghasedakpack.Ghasedak("8ccb470a1504662baa0523cc138d1c8aec749a6deed7c84278f48662af5c2f24")
+sms.verification({'receptor': '09194207009','type': '1','template': 'randomcode','param1': 'test'})
